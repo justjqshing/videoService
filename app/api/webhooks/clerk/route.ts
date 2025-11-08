5b1ff98dd594b7c8a265e9ed1461619ee4ce4b07
Josh Shire <joshshirechildren@gmail.com>
2025-11-08 15:15:35 -0700

    Added Debug

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -48,7 +48,7 @@ export async function POST(req: NextRequest) {
       const primaryEmail = (data.email_addresses?.find((e: any) => e.id === data.primary_email_address_id)?.email_address || data.email_addresses?.[0]?.email_address || null) as string | null;
       console.log(data.email_addresses)
       const checkforsoftdelete = await User.findOneAndUpdate(
-            { email: data.email_address},
+            { email: data.email_addresses[0].email_address.toLowerCase()},
             { $set: {deleted: false} },
         );
       if (checkforsoftdelete) {return;}
