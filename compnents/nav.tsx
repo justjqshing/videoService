dec5590ee61f34488cca75881be1eb9ba90935b0
Josh Shire <joshshirechildren@gmail.com>
2025-11-08 15:45:46 -0700

    fixes

--- compnents/nav.tsx
+++ compnents/nav.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Image from "next/image";
-import Link from 'next/link'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 const Nav = () => {
     return (
         <div className={'flex flex-row justify-between w-100vw px-10 py-5'}>
-            <Image src={'./vercel.svg'} alt={'alt'} width={'50'} height={'50'}></Image>
-            <div className={'content-center justify-center flex'}>
-                <Link href="/login" className={'content-center bg-amber-700 rounded-3xl px-5 py-2 hover:bg-amber-600 duration-150'}>Login</Link>
-
+            <Image src={'/vercel.svg'} alt={'Vercel Logo'} width={50} height={50} />
+            <div className={'content-center justify-center flex gap-3'}>
+                <SignedOut>
+                    <SignInButton mode="modal">
+                        <button className={'content-center bg-amber-700 rounded-3xl px-5 py-2 hover:bg-amber-600 duration-150'}>
+                            Sign in
+                        </button>
+                    </SignInButton>
+                    <SignUpButton mode="modal">
+                        <button className={'content-center bg-amber-700 rounded-3xl px-5 py-2 hover:bg-amber-600 duration-150'}>
+                            Sign up
+                        </button>
+                    </SignUpButton>
+                </SignedOut>
+                <SignedIn>
+                    <Link href={'/dashboard'} className={'content-center bg-amber-700 rounded-3xl px-5 py-2 hover:bg-amber-600 duration-150'}>
+                        Dashboard
+                    </Link>
+                    <UserButton/>
+                </SignedIn>
             </div>
         </div>
     )
